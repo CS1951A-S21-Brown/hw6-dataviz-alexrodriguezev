03626daacd8f26985677ade500d3ef70730b556c
Alex Rodriguez <48000978+alexrodriguez0@users.noreply.github.com>
2021-03-27 20:31:37 -0400

    add mapreduce

--- preprocessing.py
+++ preprocessing.py
@@ -15,40 +15,74 @@ nodes_data = pd.DataFrame(actors, columns=['name'])
 nodes_data = nodes_data.drop_duplicates(subset=['name'])
 id_col = np.arange(len(nodes_data['name']))
 nodes_data['id'] = id_col
+# print(nodes_data)
 
 # turn nodes to json 
 nodes_data_json = nodes_data.to_json(orient="records")
 parsed = json.loads(nodes_data_json)
 nodes_data_json = json.dumps(parsed)
 nodes_data_json = {'nodes' : nodes_data_json}
-print(nodes_data_json)
 
 # make links dictionary
+
+def tryconvert(x):
+    try:
+        return nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0]
+    except:
+        print(x)
+        print(nodes_data[x])
+        print(nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0])
+    return "default"
+
 links_data = []
 
-# actors = np.array(list(map(lambda x: x.split(', ') if not isinstance(x, float) else [], netflix_data['cast'])))
-# # cast_data = map(lambda x: nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0], cast_data)
-# actors = np.apply_along_axis(map(lambda x: nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0], actors), 0, actors)
-# print("cast data", actors)
-for cast in netflix_data['cast']:
-    actors = cast.split(', ') if not isinstance(cast, float) else []
-    for i in range(len(actors)):
-        for j in range(i + 1, len(actors)):
-            actor1 = actors[i]
-            actor2 = actors[j]
-            id1 = nodes_data.loc[nodes_data['name'] == actor1, 'id'].iloc[0]
-            id2 = nodes_data.loc[nodes_data['name'] == actor2, 'id'].iloc[0]
-            links_data.append([id1, id2])
+casts = np.array(list(map(lambda x: x.split(', ') if not isinstance(x, float) else [], netflix_data['cast'])))
+# new_df = pd.DataFrame(actors, columns=['cast'])
+# new_df = new_df.applymap(lambda x: nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0])
+# print(new_df)
+
+# pairs = []
+
+links_data = map(lambda x: list(combinations(x, 2)), list(casts))
+# print(list(links_data))
+links_data = list(reduce(lambda a, b: a + b, list(links_data)))
+# links_data = pd.DataFrame(links_data, columns=['source', 'target'])
+print(links_data)
+links_data = list(map(lambda x: [nodes_data.loc[nodes_data['name'] == x[0], 'id'].iloc[0], \
+    nodes_data.loc[nodes_data['name'] == x[1], 'id'].iloc[0]], links_data.values))
+
 links_data = pd.DataFrame(links_data, columns=['source', 'target'])
-links_data = links_data.drop_duplicates(subset=['source', 'target']) 
+
+# links_data = links_data.reshape(-1, links_data.shape[-1])
+
 print(links_data)
 
-# turn nodes to json 
-links_data_json = links_data.to_json(orient="records")
-parsed = json.loads(links_data_json)
-links_data_json = json.dumps(parsed)
-links_data_json = {'nodes' : links_data_json}
-print(links_data_json)
+# for actors in casts:
+#     pairs.append(combinations(actors))
+# links_data = np.array(links_data).flatten()
+# links_data = pd.DataFrame(links_data, columns=['source', 'target']) 
+# # cast_data = map(lambda x: nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0], cast_data)
+# actors = np.apply_along_axis(map(lambda x: nodes_data.loc[nodes_data['name'] == x, 'id'].iloc[0], actors), 0, actors)
+# # print("cast data", actors)
+# for cast in netflix_data['cast']:
+#     actors = cast.split(', ') if not isinstance(cast, float) else []
+#     for i in range(len(actors)):
+#         for j in range(i + 1, len(actors)):
+#             actor1 = actors[i]
+#             actor2 = actors[j]
+#             id1 = nodes_data.loc[nodes_data['name'] == actor1, 'id'].iloc[0]
+#             id2 = nodes_data.loc[nodes_data['name'] == actor2, 'id'].iloc[0]
+#             links_data.append([id1, id2])
+# links_data = pd.DataFrame(links_data, columns=['source', 'target'])
+# links_data = links_data.drop_duplicates(subset=['source', 'target']) 
+# print(links_data)
+
+# # turn nodes to json 
+# links_data_json = links_data.to_json(orient="records")
+# parsed = json.loads(links_data_json)
+# links_data_json = json.dumps(parsed)
+# links_data_json = {'nodes' : links_data_json}
+# print(links_data_json)
 
 # for cast in netflix_data['cast']:
 #     cast_names = cast.split(', ')
